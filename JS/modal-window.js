2d40f3944afeaf95e84bf5417283e2417727bcd9
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-07-29 20:11:11 +0200

    .

--- JS/modal-window.js
+++ JS/modal-window.js
@@ -12,7 +12,34 @@ document.addEventListener("DOMContentLoaded", function() {
         closeBtn.addEventListener("click", () => {
             backdrop.classList.add("is-closed");
         });
+
+
     } else {
         console.error("One or more elements are missing. Check the selectors and the HTML structure.");
     }
 });
+
+// const escKeydown = documentQuerySelector(e.key == "Escape");
+// document.addEventListener('keydown', function (event) {
+
+//     if (event.key === 'Escape') {
+// const backdrop = document.querySelector(".backdrop");
+
+//       backdrop.classList.add("is-closed");
+//   }
+// })
+
+
+// const backdrop = document.querySelector(".backdrop");
+// document.addEventListener("keydown", function (e) {
+//     if (e.key == "Escape") {
+//         backdrop.classList.add("is-closed");
+//      }
+// })
+ 
+
+// function keyPress(e) {
+//     if(e.key === "Escape") {
+//         backdrop.classList.add("is-closed");
+//     }
+// }
